77d9f7418364dc144a69d9e4e5d33b013d8638e5
gyokuro <davidc616@gmail.com>
2012-01-01 16:04:49 -0800

    bigmemory

---
+++ R/bigmemory_4.2.11/doc/Overview.R
@@ -0,0 +1,8 @@
+### R code from vignette source 'Overview.Rnw'
+
+###################################################
+### code chunk number 1: setup
+###################################################
+options(keep.source = TRUE, width = 75)
+
+
