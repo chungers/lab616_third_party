f81a66c1cdc9f793b0d0eafc1a584fa288a07fa7
David <davidc616@gmail.com>
2012-02-12 21:12:47 -0800

    Adding dependencies for lubridate

--- R/install.sh
+++ R/install.sh
@@ -6,5 +6,5 @@ R CMD INSTALL int64_1.1.2.tar.gz
 R CMD INSTALL bigmemory_4.2.11.tar.gz 
 R CMD INSTALL zoo_1.7-6.tar.gz xts_0.8-2.tar.gz TTR_0.21-0.tar.gz Defaults_1.1-1.tar.gz quantmod_0.3-17.tar.gz 
 R CMD INSTALL bit_1.1-7.tar.gz ff_2.2-3.tar.gz 
-R CMD INSTALL lubridate_0.2.6.tar.gz 
+R CMD INSTALL plyr_1.7.1.tar.gz stringr_0.6.tar.gz lubridate_0.2.6.tar.gz 
 
